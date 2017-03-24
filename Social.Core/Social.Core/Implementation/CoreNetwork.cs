2bf57cb62d30e82f98fbf677fdade98403f0f30e
Ylli Prifti <yprifti@hotmail.com>
2017-03-24 00:30:59 +0000

    log can be null

--- Social.Core/Social.Core/Implementation/CoreNetwork.cs
+++ Social.Core/Social.Core/Implementation/CoreNetwork.cs
@@ -24,11 +24,11 @@ namespace Social.Core.Implementation
 
             if (!_storage.Exists(subscribeTo))
             {
-                _log.Warn(string.Format("User {0} to subscribe to does not exist", subscribeTo.UserName));
+                _log?.Warn(string.Format("User {0} to subscribe to does not exist", subscribeTo.UserName));
                 return;
             }
             _storage.Subscribe(subscriber, subscribeTo);
-            _log.Log(string.Format("{0} subscribed to {1}", subscriber.UserName, subscribeTo.UserName));
+            _log?.Log(string.Format("{0} subscribed to {1}", subscriber.UserName, subscribeTo.UserName));
         }
 
         public void Post(IUser user, IMessage message)
@@ -36,18 +36,18 @@ namespace Social.Core.Implementation
             CheckExists(user);
             message.User = user;
             _storage.Post(user, message);
-            _log.Log(string.Format("{0} Message posted", message.MessageText));
+            _log?.Log(string.Format("{0} Message posted", message.MessageText));
         }
 
         public IEnumerable<IMessage> Read(IUser viewing)
         {
             if (!_storage.Exists(viewing)) {
-                _log.Warn(string.Format("{0} does not exist", viewing.UserName));
+                _log?.Warn(string.Format("{0} does not exist", viewing.UserName));
                 return null;
             }
             var items = _storage.Read(viewing);
             if (items != null && items.Any())
-                _log.Write(string.Join(Environment.NewLine, items.AsParallel().Select(x => x.CompactToString())));
+                _log?.Write(string.Join(Environment.NewLine, items.AsParallel().Select(x => x.CompactToString())));
             return items;
 
         }
@@ -57,7 +57,7 @@ namespace Social.Core.Implementation
             CheckExists(userDashboard);
             var items = _storage.Wall(userDashboard);
             if (items != null && items.Any())
-                _log.Write(string.Join(Environment.NewLine, items.AsParallel().Select(x => x.ToString())));
+                _log?.Write(string.Join(Environment.NewLine, items.AsParallel().Select(x => x.ToString())));
             return items;
         }
 
@@ -65,7 +65,7 @@ namespace Social.Core.Implementation
             if (!_storage.Exists(user))
             {
                 _storage.Add(user);
-                _log.Log(string.Format("User {0} added", user.UserName));
+                _log?.Log(string.Format("User {0} added", user.UserName));
             }
 
         }
